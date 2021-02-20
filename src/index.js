850abdf8c73801b65ca2fc5fe321a7b3cb016353
Aftabb alamm <aftab1234011@gmail.com>
2021-02-19 22:07:42 -0500

    "update: registered service worker in index file"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './css/main.css';
 import './css/responsive.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -12,8 +15,13 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
