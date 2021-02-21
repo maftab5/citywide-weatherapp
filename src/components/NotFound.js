05229ccc4b37415e60cd6ab09743202173dae502
Aftabb alamm <aftab1234011@gmail.com>
2021-02-21 10:28:04 -0500

    "Feature: added not found page"

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable linebreak-style */
+const NotFound = () => (
+  <>
+    <div className="notfound-card">
+      <section>
+        <h3>
+          Something Went wront. Please try again
+        </h3>
+      </section>
+    </div>
+  </>
+);
+export default NotFound;
