5407b4fe34e73cde698c13298052fa47c2f5f8f7
Aftabb alamm <aftab1234011@gmail.com>
2021-02-21 10:51:57 -0500

    "feature: NotFound component added"

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -13,62 +13,64 @@
  */
 import PropTypes from 'prop-types';
 import { kelvinToCelsius, convertTimestamp, getWeatherImage } from '../functions/function';
+import NotFound from './NotFound';
 /**
  * Component to display
  * the weather information
  */
 
-const WeatherInfo = ({ weatherInfo }) => (
+const WeatherInfo = ({ weatherInfo, error }) => (
   <>
 
     <div className="flex-container">
-      {
-      weatherInfo.map(({
-        name, main, sys, weather, wind,
-      }, index) => (
-
+      { error ? <NotFound /> : (
+        weatherInfo.map(({
+          name, main, sys, weather, wind,
+        }, index) => (
         // eslint-disable-next-line react/no-array-index-key
-        <div className="weather-item" key={index}>
-          <div className="card">
-            <section className="temp_section">
-              <p className="main_temperature">
-                {' '}
-                {kelvinToCelsius(main.temp)}&deg;
-              </p>
-              <h3 className={`location${index} `}>
-                {name}, {sys.country}
-              </h3>
-            </section>
+          <div className="weather-item" key={index}>
+            { name ? (
+              <div className="card">
+                <section className="temp_section">
+                  <p className="main_temperature">
+                    {kelvinToCelsius(main.temp)}&deg;
+                    <img
+                      src={getWeatherImage(weather[0].icon)}
+                      alt="weather icon"
+                      height="75"
+                      width="38"
+                    />
+                  </p>
+                  <h3 className={`location${index} `}>
+                    {name}, {sys.country}
+                  </h3>
+                </section>
 
-            <section className="card-body">
-              <img
-                src={getWeatherImage(weather[0].icon)}
-                alt="weather icon"
-                height="60"
-                width="30"
-              />
+                <section className="card-body">
 
-              <div className="card-text">
-                {kelvinToCelsius(main.temp_max)}&deg;
-                |
-                {kelvinToCelsius(main.temp_min)}&deg;
-                <p>
-                  Feels like: {kelvinToCelsius(main.feels_like)}&deg;
-                </p>
-                <p> Wind: {wind.speed} mph </p>
-                <p> Sunrise: {convertTimestamp((sys.sunrise))} AM </p>
-                <p> Sunset: {convertTimestamp(sys.sunset)} PM </p>
+                  <div className="card-text">
+                    <p> {kelvinToCelsius(main.temp_max)}&deg;
+                      |
+                      {kelvinToCelsius(main.temp_min)}&deg;
+                    </p>
+                    <p>
+                      Feels like: {kelvinToCelsius(main.feels_like)}&deg;
+                    </p>
+                    <p> Wind: {wind.speed} mph </p>
+                    <p> Sunrise: {convertTimestamp((sys.sunrise))} AM </p>
+                    <p> Sunset: {convertTimestamp(sys.sunset)} PM </p>
+                  </div>
+                </section>
               </div>
-            </section>
+            ) : <NotFound />};
           </div>
-        </div>
 
-      ))
-}
+        )))}
     </div>
   </>
 );
 WeatherInfo.propTypes = {
   weatherInfo: PropTypes.array.isRequired,
+  error: PropTypes.string.isRequired,
 };
 export default WeatherInfo;
